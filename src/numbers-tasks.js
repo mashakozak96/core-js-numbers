adef396a27e9a140eb013c0a75c2baa284a58f36
mashakozak96 <mashakozak96@gmail.com>
2025-09-22 22:19:29 +0300

    feat: add getNumberValue and isNumber tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -392,8 +392,8 @@ function toPrecision(number, precision) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  return number.valueOf();
 }
 
 /**
@@ -411,8 +411,8 @@ function getNumberValue(/* number */) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  return Number.isFinite(number);
 }
 
 /**
